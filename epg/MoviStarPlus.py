cc6ecc3cdeb333c1449eefc91564bfbd346352bc
rule-set <153180091+rule-set@users.noreply.github.com>
2025-02-01 02:29:14 +0800

    Update MoviStarPlus.py

--- epg/MoviStarPlus.py
+++ epg/MoviStarPlus.py
@@ -1,67 +1,44 @@
 from loguru import logger
-from .utils import (
-    convert_date_string,
-    generate_formatted_date_range,
-)
+from .utils import convert_date_string, generate_formatted_date_range
 import requests
 
 
 class UpdateFromMoviStarPlus:
     def __init__(self):
-        self.base_url = "https://movistarplus.rule-set.workers.dev/?date={date}"
-
+        self.base_url = "https://movistarplus.rule-set.workers.dev/?id={id}&date={date}"
         self.date_fmt = "%Y-%m-%d %H:%M"
         self.delta = 6
 
     def fetch_programs(self, id):
         programmes = []
 
-        dates = generate_formatted_date_range(
-            start_days_ago=7, end_days_in_future=6, fmt="%Y-%m-%d", tz="Europe/Madrid"
-        )
-        for date in dates:
+        for date in generate_formatted_date_range(start_days_ago=7, end_days_in_future=6, fmt="%Y-%m-%d", tz="Europe/Madrid"):
             try:
-                epg_json = requests.get(
-                    self.base_url.format(date=date), verify=False
-                ).json()
-
-                epg_item = epg_json["data"][id]["PROGRAMAS"]
+                epg_json = requests.get(self.base_url.format(id=id, date=date), verify=False).json()
+                item_list = next((item for item in epg_json if item.get("@type") == "ItemList"), None)
+                if not item_list:
+                    logger.warning(f"No ItemList found for channel {id} on date {date}.")
+                    continue
 
                 programmes.extend(
-                    [
-                        {
-                            "start": convert_date_string(
-                                f'{date} {item["HORA_INICIO"]}',
-                                self.date_fmt,
-                                self.delta,
-                            ),
-                            "stop": convert_date_string(
-                                f'{date} {item["HORA_FIN"]}',
-                                self.date_fmt,
-                                self.delta,
-                            ),
-                            "title": item["TITULO"],
-                        }
-                        for item in epg_item
-                    ]
+                    {
+                        "start": convert_date_string(item["item"]["startDate"], self.date_fmt, self.delta),
+                        "stop": convert_date_string(item["item"]["endDate"], self.date_fmt, self.delta),
+                        "title": item["item"]["name"],
+                    }
+                    for item in item_list.get("itemListElement", [])
+                    if all(key in item.get("item", {}) for key in ["startDate", "endDate", "name"])
                 )
+
             except requests.RequestException as e:
                 logger.error(f"Request error for channel {id}: {e}")
-                continue
             except Exception as e:
                 logger.error(f"An unexpected error occurred for channel {id}: {e}")
-                continue
 
-        if programmes:
-            logger.info(
-                f"Successfully fetched {len(programmes)} programs for channel {id}."
-            )
-        else:
-            logger.warning(f"No programme information was fetched for {id}.")
-
+        logger.info(f"Fetched {len(programmes)} programs for channel {id}." if programmes else f"No programmes fetched for {id}.")
         return programmes
 
 
 if __name__ == "__main__":
     updater = UpdateFromMoviStarPlus()
-    updater.fetch_programs("MVF1-CODE")
+    updater.fetch_programs("mvf1")
